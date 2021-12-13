ca6b28c7c6201d5c7979f4e5401a8c4cc6bd8401
makfazlic <makfazlic@protonmail.com>
2021-12-13 23:45:53 +0100

    Starting the Features section of the landing page

--- volonpeer-project/src/App.js
+++ volonpeer-project/src/App.js
@@ -1,12 +1,14 @@
 import './css/App.css';
 import Header from './components/Header';
 import Jumbotron from './components/Jumbotron';
+import Features from './components/Features';
 
 function App() {
   return (
     <div>
       <Header />
       <Jumbotron />
+      <Features />
     </div>
 
 
---
+++ volonpeer-project/src/components/Features.js
@@ -0,0 +1,9 @@
+export default function Features() {
+  return (
+
+    <div className="features">
+        <p>Hello</p>
+    </div>
+
+    );
+}
